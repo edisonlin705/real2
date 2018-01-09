8ea2e2cba972cbb81e477a20cf85b1a0ddbb69dd
edisonlin705 <edisonlin705@gmail.com>
2018-01-10 02:05:28 +0800

    完成2

---
+++ fktest/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String name = textBox1.Text;
+            String phone = textBox2.Text;
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+            TextBlock tb = new TextBlock();
+            tb.Inlines.Add(new Bold(new Run(name)));
+            tb.Inlines.Add(new Italic(new Run(phone)));
+
+            stackPanell.Children.Add(tb);
+        }
+    }
+}
---
+++ fktest/WpfApp6/WpfApp6/Window1.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Window1.xaml 的互動邏輯
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        public InkCanvas mydraw; 
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingAttributes da = new DrawingAttributes();
+            da.Color = Colors.Blue;
+            da.Width = 10;
+            mydraw.DefaultDrawingAttributes = da;
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingAttributes da = new DrawingAttributes();
+            da.Color = Colors.Orange;
+            da.Width = 10;
+            mydraw.DefaultDrawingAttributes = da;
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingAttributes da = new DrawingAttributes();
+            da.Color = Colors.Red;
+            da.Width = 15;
+            mydraw.DefaultDrawingAttributes = da;
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingAttributes da = new DrawingAttributes();
+            da.Color = Colors.Purple;
+            da.Width = 20;
+            mydraw.DefaultDrawingAttributes = da;
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingAttributes da = new DrawingAttributes();
+            da.Color = Colors.Yellow;
+            da.Width = 10;
+            mydraw.DefaultDrawingAttributes = da;
+        }
+
+        private void button6_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingAttributes da = new DrawingAttributes();
+            da.Color = Colors.Green;
+            da.Width = 10;
+            mydraw.DefaultDrawingAttributes = da;
+        }
+
+        private void button7_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingAttributes da = new DrawingAttributes();
+            da.Color = Colors.Gray;
+            da.Width = 25;
+            mydraw.DefaultDrawingAttributes = da;
+        }
+
+        private void button8_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingAttributes da = new DrawingAttributes();
+            da.Color = Colors.Black;
+            da.Width = 10;
+            mydraw.DefaultDrawingAttributes = da;
+        }
+
+        private void button9_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingAttributes da = new DrawingAttributes();
+            da.Color = Colors.DarkBlue;
+            da.Width = 5;
+            mydraw.DefaultDrawingAttributes = da;
+        }
+
+        private void button10_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingAttributes da = new DrawingAttributes();
+            da.Color = Colors.Pink;
+            da.Width = 3;
+            mydraw.DefaultDrawingAttributes = da;
+        }
+    }
+}
